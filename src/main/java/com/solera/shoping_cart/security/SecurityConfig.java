6aea35f16bc6dae81501794b1d3ae51bde8a91c8
Julieta <julieta.vargas@solera.com>
2025-07-25 15:25:26 -0600

    Security spring added

--- src/main/java/com/solera/shoping_cart/security/SecurityConfig.java
+++ src/main/java/com/solera/shoping_cart/security/SecurityConfig.java
@@ -1,22 +1,51 @@
 package com.solera.shoping_cart.security;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/products/**").hasRole("ADMIN")
+                .requestMatchers("/api/cart-items/**").hasAnyRole("ADMIN", "USER")
+                .requestMatchers("/api/users/**").hasRole("ADMIN")
+                .requestMatchers("/api/carts/**").hasAnyRole("ADMIN", "USER")
+                .anyRequest().authenticated()
+            )
+            .httpBasic(Customizer.withDefaults()); // Autenticación básica
+        return http.build();
+    }
 
-   @Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http
-        .csrf(csrf -> csrf.disable()) // Desactiva CSRF para facilitar pruebas
-        .authorizeHttpRequests(auth -> auth
-            .anyRequest().permitAll() // Permitir todo sin autenticación
-        )
-        .httpBasic(httpBasic -> httpBasic.disable()); // Desactiva completamente auth básica
-    return http.build();
-}
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername("user")
+            .password(passwordEncoder.encode("userpass"))
+            .roles("USER")
+            .build();
+
+        UserDetails admin = User.withUsername("admin")
+            .password(passwordEncoder.encode("adminpass"))
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
